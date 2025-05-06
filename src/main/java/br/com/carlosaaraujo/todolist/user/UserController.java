b5f385218dbc3423e2fb41ec7a72176488fcc236
Carlos Araújo <carlosaaraujopc@gmail.com>
2025-05-06 20:11:57 -0300

    feat: implement user registration endpoint with password hashing and validation

--- src/main/java/br/com/carlosaaraujo/todolist/user/UserController.java
+++ src/main/java/br/com/carlosaaraujo/todolist/user/UserController.java
@@ -1,4 +1,34 @@
 package br.com.carlosaaraujo.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private final IUserRepository userRepository;
+
+    public UserController(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity create (@RequestBody UserModel userModel) {
+     var user = this.userRepository.findByUsername((userModel.getUsername()));
+
+     if (user != null) {
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+     }
+
+     var passwordHashhred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+     userModel.setPassword(passwordHashhred);
+
+     var userCreated = this.userRepository.save(userModel);
+     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
 }
