fc1297da70cfbb0fe9fe3c07fd18f91772bb2b66
Carlos Araújo <carlosaaraujopc@gmail.com>
2025-05-06 20:11:48 -0300

    feat: implement TaskController with task creation logic and validation

--- src/main/java/br/com/carlosaaraujo/todolist/task/TaskController.java
+++ src/main/java/br/com/carlosaaraujo/todolist/task/TaskController.java
@@ -1,4 +1,38 @@
 package br.com.carlosaaraujo.todolist.task;
 
-public class TaskController {
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController  {
+    private final ITaskRepository taskRepository;
+
+    public TaskController(ITaskRepository taskRepository) { this.taskRepository = taskRepository; }
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid date");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Start date must be before end date");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
 }
